a942d38f5b8f916b7dd3a7873410057c5af509e1
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-12-21 20:35:58 -0300

    Improving Views (p1)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,17 +2,23 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
-# Create your views here.
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -33,7 +39,7 @@ def vote(request, question_id):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "Você não selecionou uma escolha.",})
+            'error_message': "Você não selecionou uma escolha.", })
     else:
         selected_choice.votes += 1
         selected_choice.save()
